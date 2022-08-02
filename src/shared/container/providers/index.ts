6bfab855c6788fde537e33bdfcfe733cfbff3644
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-08-02 18:49:35 -0300

    feat: registering axios provider instance

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,12 @@ import { container } from 'tsyringe';
 
 import { IDateProvider } from './DateProvider/IDateProvider';
 import { DayjsDateProvider } from './DateProvider/implementations/DayjsDateProvider';
+import { IAxiosProvider } from './HttpClientProvider/IAxiosProvider';
+import { AxiosProvider } from './HttpClientProvider/implementations/AxiosProvider';
 
 container.registerSingleton<IDateProvider>(
   'DayjsDateProvider',
   DayjsDateProvider
 );
+
+container.registerSingleton<IAxiosProvider>('AxiosProvider', AxiosProvider);
